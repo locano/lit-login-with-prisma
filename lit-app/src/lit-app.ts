3698b2e0778077662e3e961da55c5e4fc6089f1a
Ludwing Cano <ludwing238@gmail.com>
2025-03-19 13:18:59 -0600

    feat(login) : adding new login and welcome screen

--- lit-app/src/lit-app.ts
+++ lit-app/src/lit-app.ts
@@ -1,16 +1,48 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import './welcome/welcome';
+import './login/login';
 
 @customElement('lit-app')
 export class LitApp extends LitElement {
 
+    static styles = [
+        css`
+            :host {
+                display: block;
+            }
 
+            .container {
+                color: #fff;
+                background-color: #333;
+            }    
+        `
+    ];
 
     @property() title = 'Lit App';
+    @property() token = '';
+
+
+
 
     render() {
         return html`
-            <h1>${this.title}</h1>
+        <section class="container">
+        ${this.token ? html`<welcome-page @signout=${this.handleSignout}></welcome-page>` : html`
+           <login-page @signin=${this.handleSignIn}></login-page>
+        `}
+        </section>
         `;
+    }
+
+
+    handleSignIn(event: CustomEvent) {
+        console.log('handleSignIn', event.detail.token);
+        this.token = event.detail.token;
+    }
+
+    handleSignout(event: CustomEvent) {
+        console.log('handleSignout', event.detail.token);
+        this.token = event.detail.token;
     }
 }
